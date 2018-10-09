9f8f24f624a55743d3b9d1bd5929268a1828053e
cattermo <clara.attermo@gmail.com>
2018-10-09 22:52:15 +0200

    ftp

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 npm run generate
 cd out
 ls
-find * -type f -exec curl -u $FTP_USER:$FTP_PASSWORD --ftp-create-dirs -T {} sftp://ssh.thebandettes.com/{} \;
+find * -type f -exec curl -u $FTP_USER:$FTP_PASSWORD --ftp-create-dirs -T {} ftp://ssh.thebandettes.com/{} \;
